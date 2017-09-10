d9221079cc4cac3ece0a655bdfa5ae54730e4baa
海老原祐輔 <ebiyuu1121@gmail.com>
2017-09-11 06:23:36 +0900

    [delete] link of unnecessary file

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim  ~/.vim
-ln -sf ~/dotfiles/.bash_sessions ~/.bash_sessions
-ln -sf ~/dotfiles/.bash_history ~/.bash_history
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zsh_history ~/.zsh_history
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
