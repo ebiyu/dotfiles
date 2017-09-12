4d5eb0527c0f43d301e9aef04e17822c0c2b3068
海老原祐輔 <ebiyuu1121@gmail.com>
2017-09-12 15:02:36 +0900

    [fix]

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -5,4 +5,3 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tw.yml ~/.tw.yml
