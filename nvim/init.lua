37be444b1a817ffc61c0f1f515236c941e2b9b80
Ebihara Yusuke <yusuke@ebihara.me>
2023-05-11 00:43:55 +0900

    M_nvim/init.lua M_nvim/lazy-lock.json

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,14 +11,14 @@ tnoremap <esc> <c-\><c-n>
 -- Setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
@@ -27,12 +27,99 @@ require("lazy").setup({
     {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
-    }
+    },
+    "neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+    },
+    "williamboman/mason-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
 })
 
 -- fern
-vim.api.nvim_set_keymap('n', '<C-b>', ':Fern . -reveal=% <CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-b>', '<cmd>Fern . -reveal=% <CR>', { noremap = true, silent = true })
 
 -- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<c-p>', builtin.find_files, {})
+
+-- nvim-cmp
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            require 'luasnip'.lsp_expand(args.body)
+        end
+    },
+
+    mapping = {
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ['<c-n>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ['<c-p>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end
+    },
+
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'path' },
+        { name = 'buffer' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'cmdline' },
+        { name = 'git' },
+    }),
+}
+
+-- lsp
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
+
+require("mason").setup()
+require('mason-lspconfig').setup_handlers {
+    function(server_name)
+        local opts = {
+            capabilities = capabilities,
+        }
+        if server_name == "lua_ls" then
+            opts.settings = {
+                Lua = {
+                    diagnostics = { globals = { 'vim' } },
+                }
+            }
+        end
+        lspconfig[server_name].setup(opts)
+    end,
+}
+
+vim.keymap.set('n', '<space><space>', '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
