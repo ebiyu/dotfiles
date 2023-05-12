c56a9b816dbfa1ea3851ae15cf2e5379ae248106
Ebihara Yusuke <yusuke@ebihara.me>
2023-05-12 12:36:05 +0900

    M_nvim/init.lua M_nvim/lazy-lock.json

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,17 @@
-vim.api.nvim_exec([[
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-source $HOME/.vim/vimrc
-set inccommand=split
-tnoremap <esc> <c-\><c-n>
-"set cmdheight=0
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
 
-]], false)
+-- load base vimrc
+vim.api.nvim_exec([[source $HOME/.vim/vimrc]], false)
+
+vim.o.inccommand = "split"
+vim.o.laststatus = 3
+vim.o.cmdheight = 0
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
 
 -- Setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -15,18 +21,53 @@ if not vim.loop.fs_stat(lazypath) then
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    "lambdalisue/fern.vim",
+    -- colorscheme
+    {
+        "nvimdev/zephyr-nvim",
+        requires = { 'nvim-treesitter/nvim-treesitter', opt = true },
+        config = function()
+            require('zephyr')
+        end
+    },
+    'editorconfig/editorconfig-vim',
     "tpope/vim-surround",
+
+    -- filer
+    -- fern
+    -- {
+    --     "lambdalisue/fern.vim",
+    --     config = function()
+    --         vim.keymap.set('n', '<C-b>', '<cmd>Fern . -reveal=% <CR>')
+    --     end
+    -- },
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            require('nvim-tree').setup {
+                view = {
+                    float = {
+                        enable = false,
+                    }
+                }
+            }
+            vim.keymap.set('n', '<C-b>', '<cmd>NvimTreeFindFile<CR>')
+        end
+    },
+    "nvim-tree/nvim-web-devicons",
+
+    -- file finder
     {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
+
+    -- lsp
     "neovim/nvim-lspconfig",
     {
         "williamboman/mason.nvim",
@@ -35,15 +76,22 @@ require("lazy").setup({
     "williamboman/mason-lspconfig",
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
-    -- "tpope/vim-commentary",
+
     {
         "nvim-treesitter/nvim-treesitter",
     },
     {
-        "nvimdev/zephyr-nvim",
-        requires = { 'nvim-treesitter/nvim-treesitter', opt = true },
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup {
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    delay = 0,
+                    ignore_whitespace = false,
+                },
+            }
+        end
     },
-    "airblade/vim-gitgutter",
     {
         "folke/which-key.nvim",
         config = function()
@@ -65,11 +113,6 @@ require("lazy").setup({
     "RRethy/vim-illuminate",
 })
 
--- colorscheme
-require('zephyr')
-
--- fern
-vim.keymap.set('n', '<C-b>', '<cmd>Fern . -reveal=% <CR>', { noremap = true, silent = true })
 
 -- telescope
 local builtin = require('telescope.builtin')
@@ -125,7 +168,25 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
 require("mason").setup()
-require('mason-lspconfig').setup_handlers {
+
+local masonconfig = require('mason-lspconfig')
+masonconfig.setup {
+    ensure_installed = {
+        'bashls',
+        'jedi_language_server',
+        'gopls',
+        'lua_ls',
+        'quick_lint_js',
+        'tsserver',
+        'jsonls',
+        'html',
+        'cssls',
+        'eslint',
+    },
+    automatic_installation = {},
+}
+
+masonconfig.setup_handlers {
     function(server_name)
         local opts = {
             capabilities = capabilities,
@@ -138,15 +199,16 @@ require('mason-lspconfig').setup_handlers {
             }
         end
         lspconfig[server_name].setup(opts)
+
+        -- setup keymaps handlers
+        vim.keymap.set('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = true })
     end,
 }
-
 vim.keymap.set('n', '<space><space>', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format { async = true }<CR>')
 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
 vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
@@ -156,8 +218,10 @@ vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
 vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 
+
 -- tree sitter
 require 'nvim-treesitter.configs'.setup {
+    ensure_installed = "all",
     highlight = {
         enable = true,
 
