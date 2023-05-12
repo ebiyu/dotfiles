384c6cbc2c5bb64cb2ceacbd19a3569127f34c68
Ebihara Yusuke <yusuke@ebihara.me>
2023-05-12 20:50:12 +0900

    auto commit

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,6 +8,11 @@ vim.opt.termguicolors = true
 -- load base vimrc
 vim.api.nvim_exec([[source $HOME/.vim/vimrc]], false)
 
+-- chezmoi auto apply
+vim.api.nvim_exec([[
+    autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
+]], false)
+
 vim.o.inccommand = "split"
 vim.o.laststatus = 3
 vim.o.cmdheight = 0
@@ -323,3 +328,4 @@ require 'nvim-treesitter.configs'.setup {
         enable = true,
     },
 }
+
