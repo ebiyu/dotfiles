e8adf6a28edf398679dbd69e3a165ab4d9a72633
Ebihara Yusuke <yusuke@ebihara.me>
2023-05-14 17:49:37 +0900

    Add .config/wezterm/keybinds.lua Add .config/wezterm/keybinds_default.lua Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,4 +1,9 @@
 return {
-    font_size = 12.0,
+    leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 2000 },
+    keys = require('keybinds').keys,
+    key_tables = require('keybinds').key_tables,
+    disable_default_key_bindings = true,
+    font_size = 10.0,
     use_ime = false,
 }
+
