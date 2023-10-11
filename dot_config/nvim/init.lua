2d8177bad65949375b06569a55aa65c9805e0117
Ebihara Yusuke <yusuke@ebihara.me>
2023-10-11 18:58:44 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -316,16 +316,7 @@ require("lazy").setup({
     },
     "RRethy/vim-illuminate",
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("indent_blankline").setup {
-                -- for example, context is off by default, use this to turn it on
-                show_current_context = true,
-                -- show_current_context_start = true,
-            }
-        end
-    },
+    {"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}},
 
     {
         "vim-skk/skkeleton",
